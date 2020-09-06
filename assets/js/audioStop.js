291ca7cacdca122e410c6143b5b532bb23384d28
ekosistema <46524536+ekosistema@users.noreply.github.com>
2020-09-06 10:57:48 +0200

    javascript files combined
    javascript files combined

--- assets/js/audioStop.js
+++
@@ -1,7 +0,0 @@
-function detenerAudio() {
-    document.addEventListener("click", async () => {
-        Tone.context.close();
-        console.log("audio stopped");
-        location.reload();
-    });
-}
